7455841ceff17092af6e17a29dbc5f940f490dac
amitramesh <amit.ramesh.sharma@gmail.com>
2019-07-16 21:41:18 +0530

    Add Owin Package

---
+++ WebAuth/WebAuth/MyAuthorizationServerProvider.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AuthWebApi
+{
+    //http://bitoftech.net/2014/10/27/json-web-token-asp-net-web-api-2-jwt-owin-authorization-server/
+
+    public class MyAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            string clientId = string.Empty;
+            string clientSecret = string.Empty;
+            string symmetricKeyAsBase64 = string.Empty;
+
+            if (context.TryGetBasicCredentials(out clientId, out clientSecret))
+            {
+                // validate the client Id and secret against database or from configuration file.  
+                if (context.ClientId == null)
+                {
+                    context.SetError("invalid_clientId", "client_Id is not set");
+
+
+                    return Task.FromResult<object>(null);
+                }
+            }
+            else
+            {
+                context.SetError("invalid_client", "Client credentials could not be retrieved from the Authorization header");
+                context.Rejected();
+                return Task.FromResult<object>(null);
+            }
+            context.Validated();
+            return Task.FromResult<object>(null);
+        }
+
+        public override Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            //return base.GrantResourceOwnerCredentials(context);
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+            if (context.UserName != "sam" && context.Password != "password")
+            {
+                context.SetError("Invalid grant", "The user name or password is incorrect");
+                return Task.FromResult<object>(null);
+            }
+            var identity = new ClaimsIdentity("JWT");
+            identity.AddClaim(new Claim(ClaimTypes.Name, context.UserName));
+            identity.AddClaim(new Claim("sam", context.UserName));
+            identity.AddClaim(new Claim(ClaimTypes.Role, "Manager"));
+            identity.AddClaim(new Claim(ClaimTypes.Role, "Superwiser"));
+
+            var props = new AuthenticationProperties(new Dictionary<string, string>
+                {
+                    {
+                         "audience", (context.ClientId == null) ? string.Empty : context.ClientId
+                    }
+                });
+
+            var ticket = new AuthenticationTicket(identity, props);
+            context.Validated(ticket);
+            return Task.FromResult<object>(null);
+        }
+    }
+}
